1fc22863f2d4908e835410c9a2eec8418fee1d34
treywwre-rgb <treyw.wre@gmail.com>
2026-07-16 21:38:45 -0500

    Finished Project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,6 @@ import logo from '../../assets/assets/logo.png';
 import search_icon from '../../assets/assets/search_icon.svg'
 import bell_icon from '../../assets/assets/bell_icon.svg'
 import profile_img from '../../assets/assets/profile_img.png'
-import caret_icon from '../../assets/assets/caret_icon.svg'
 import { logout } from '../../firebase';
 
 const Navbar = () => {
@@ -40,7 +39,7 @@ const Navbar = () => {
         <img src={bell_icon} alt="" className='icons' />
         <div className="navbar-profile">
           <img src={profile_img} alt="" className='profile' />
-          <img src={caret_icon} alt="" />
+          
           <div className="dropdown">
             <p onClick={()=>{logout()}}>Sign Out of Netflix</p>
           </div>
